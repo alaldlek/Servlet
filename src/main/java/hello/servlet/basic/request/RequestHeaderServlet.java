1b3b5aaceab9cd0f61c01f0fd72c128cb10f9441
alaldlek <tptpdlek@naver.com>
2024-11-24 19:51:54 +0900

    GET parameter 1.0.3

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
-public class requestHeaderServlet extends HttpServlet {
+public class RequestHeaderServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
